87465bbf3049a776a51626e72ebbf3ba3119b582
Anurag Ghutke <anurag135ghutke@gmail.com>
2026-01-03 20:26:13 +0530

    added updated lottery game logic

--- projects/lottery-game/script.js
+++ projects/lottery-game/script.js
@@ -2,22 +2,20 @@ const sumElm = document.getElementById('sum')
 const button = document.querySelector('button')
 const ticketNo = document.getElementById('ticket-no')
 const winElm = document.getElementById('win')
+const loseElm = document.getElementById('lose')
 const ruleElm = document.getElementById('rule')
 
 const getSum = (num) => {
     let sum = 0
-
     while (num > 0) {
         let lastDig = num % 10
         sum += lastDig
         num = Math.floor(num / 10)
     }
-
     return sum
 }
 
 button.addEventListener('click', () => {
-
     ruleElm.style.display = 'none'
 
     const ticket = Math.floor(Math.random() * 900) + 100
@@ -28,5 +26,9 @@ button.addEventListener('click', () => {
 
     if (total == 15) {
         winElm.style.display = 'block'
+        loseElm.style.display = 'none'
+    } else {
+        winElm.style.display = 'none'
+        loseElm.style.display = 'block'
     }
 })
